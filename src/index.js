54e78b527f7790a7218f9e1ef846599662d19bf1
Aco Mitevski <aco.mitevski@gmail.com>
2017-02-02 23:29:20 +0100

    use hashHistory to work with github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { browserHistory } from 'react-router'
+import { hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
@@ -8,7 +8,7 @@ import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 
 const store = configureStore()
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(hashHistory, store)
 
 render(
   <Root store={store} history={history} />,
